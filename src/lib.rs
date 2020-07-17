583ce8e8b829851678dd7160497e29a4b699003b
Andrey Zheleznov <zheland.net@gmail.com>
2020-07-17 15:23:10 +0300

    Bump package version to 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! typed-index-collections = "0.1.1"
+//! typed-index-collections = "0.1.2"
 //! ```
 //!
 //! This crate depends on the standard library by default that is useful
@@ -41,7 +41,7 @@
 //!
 //! ```toml
 //! [dependencies.typed-index-collections]
-//! version = "0.1.1"
+//! version = "0.1.2"
 //! default-features = false
 //! features = ["alloc", "impl-index-from"]
 //! ```
@@ -52,7 +52,7 @@
 //! ```toml
 //! [dependencies]
 //! derive_more = "0.99"
-//! typed-index-collections = "0.1.1"
+//! typed-index-collections = "0.1.2"
 //! ```
 #![cfg_attr(
     all(feature = "impl-index-from", any(feature = "alloc", feature = "std")),

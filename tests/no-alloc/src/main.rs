050166d051ef8942df8aa232fa2ae807bb5f972d
Andrey Zheleznov <zheland.net@gmail.com>
2020-11-19 20:26:12 +0300

    Add no-alloc and no-std tests

---
+++ tests/no-alloc/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn slice_test() {
+    use typed_index_collections::TiSlice;
+    struct K(usize);
+    let slice = &[1, 2, 3];
+    let ti_slice: &TiSlice<K, usize> = TiSlice::from_ref(slice);
+    let _ = ti_slice;
+}
---
+++ tests/no-std/src/main.rs
@@ -0,0 +1,40 @@
+#![feature(default_alloc_error_handler)]
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::panic::PanicInfo;
+use core::ptr::null_mut;
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+struct DummyAllocator;
+
+unsafe impl GlobalAlloc for DummyAllocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        null_mut()
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: DummyAllocator = DummyAllocator;
+
+#[no_mangle]
+pub extern "C" fn vec_test() {
+    use typed_index_collections::TiVec;
+    struct K(usize);
+    let vec = alloc::vec![1, 2, 3];
+    let ti_vec: TiVec<K, usize> = TiVec::from(vec);
+    let _ = ti_vec;
+}

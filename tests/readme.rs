5878051422cf1144ca077cfa431787f47473d3a1
Andrey Zheleznov <zheland.net@gmail.com>
2020-11-22 16:25:06 +0300

    Use readme-sync dev-dependency

--- tests/readme.rs
+++ tests/readme.rs
@@ -1,136 +1,31 @@
-trait StrStripPrefix {
-    fn strip_prefix(&self, prefix: &Self) -> Option<&Self>;
-}
-
-impl StrStripPrefix for str {
-    fn strip_prefix<'a, 'b>(&'a self, prefix: &'b Self) -> Option<&'a Self> {
-        let prefix_len = prefix.len();
-        if self.len() < prefix_len {
-            None
-        } else if &self.as_bytes()[0..prefix_len] == prefix.as_bytes() {
-            Some(&self[prefix_len..])
-        } else {
-            None
-        }
-    }
-}
-
-#[cfg(any(feature = "alloc", feature = "std"))]
+#[cfg(test)]
 #[test]
-fn test_readme_docs_sync() {
-    use std::fs::read_to_string;
-    use std::vec::Vec;
+fn test_readme_sync() {
+    use readme_sync::{assert_sync, CMarkDocs, CMarkReadme, Config, Package};
 
-    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-    enum LibRsDocsLine {
-        AddedTitle,
-        AddedNewlineAfterTitle,
-        No(usize),
-    }
+    let package = Package::from_path(env!("CARGO_MANIFEST_DIR").into()).unwrap();
+    let config = Config::from_package_docs_rs_features(&package);
+    let readme = CMarkReadme::from_package(&package).unwrap();
+    let docs = CMarkDocs::from_package_and_config(&package, &config).unwrap();
 
-    let pkg_name = env!("CARGO_PKG_NAME");
-    let crate_author = "zheland";
-    let pkg_name_underscore = pkg_name.replace("-", "_");
-    let doc_root_url = format!("https://docs.rs/{}/*/{}/", pkg_name, pkg_name_underscore);
-    let mut is_last_line_used = false;
+    let readme = readme
+        .remove_badges_paragraph()
+        .remove_documentation_section()
+        .disallow_absolute_repository_blob_links()
+        .unwrap()
+        .use_absolute_repository_blob_urls()
+        .unwrap();
 
-    // The only transformation to README.md is removing badges and next line after then
-    let readme_md = read_to_string("README.md").unwrap();
-    let readme_docs = readme_md
-        .split('\n')
-        .enumerate()
-        .filter_map(|(j, line)| {
-            is_last_line_used = (is_last_line_used || line != "")
-                && !line.starts_with("[![")
-                && !line.starts_with("![")
-                && line != "## Documentation"
-                && !line.starts_with("[API Documentation]");
-            if is_last_line_used {
-                Some((j + 1, line))
-            } else {
-                None
-            }
-        }) // Skip badges
-        .collect::<Vec<(usize, &str)>>();
+    let docs = docs
+        .increment_heading_levels()
+        .add_package_title()
+        .remove_codeblock_rust_test_tags()
+        .use_default_codeblock_rust_tag()
+        .remove_hidden_rust_code()
+        .disallow_absolute_package_docs_links()
+        .unwrap()
+        .use_absolute_package_docs_urls()
+        .unwrap();
 
-    // Library docs index is transformed from src/lib.rs with much transformations
-    let lib_rs = read_to_string("src/lib.rs").unwrap();
-    let mut is_code_block = false;
-    let mut is_cfg_block = false;
-    let mut lib_docs = vec![
-        (LibRsDocsLine::AddedTitle, format!("# {}", pkg_name)),
-        (LibRsDocsLine::AddedNewlineAfterTitle, String::new()),
-    ];
-    lib_docs.extend(
-        lib_rs
-            .split('\n')
-            .enumerate()
-            .filter_map(|(j, line)| {
-                StrStripPrefix::strip_prefix(line, "//! ")
-                    .or_else(|| StrStripPrefix::strip_prefix(line, "//!"))
-                    .or_else(|| {
-                        if line == "#![cfg_attr("
-                            || line == r#"    any(feature = "alloc", feature = "std"),"#
-                            || line == r#"    doc = r#""#
-                            || line == r##""#"##
-                        {
-                            is_cfg_block = true;
-                            None
-                        } else if line == ")]" {
-                            is_cfg_block = false;
-                            None
-                        } else if is_cfg_block {
-                            Some(line)
-                        } else {
-                            None
-                        }
-                    })
-                    .map(|line| (j, line))
-            })
-            .filter_map(|(j, line)| {
-                if line == "```compile_fail" {
-                    is_code_block = true;
-                    Some((LibRsDocsLine::No(j + 1), "```rust".to_string()))
-                } else if line.starts_with("```") {
-                    is_code_block = !is_code_block;
-                    Some((LibRsDocsLine::No(j + 1), line.to_string()))
-                } else if is_code_block {
-                    if line.starts_with("# ") || line == "#" {
-                        None // Skip hidden code blocks parts
-                    } else {
-                        Some((LibRsDocsLine::No(j + 1), line.to_string()))
-                    }
-                } else if line.starts_with('#') {
-                    Some((LibRsDocsLine::No(j + 1), "#".to_string() + line)) // Add extra "#" for titles
-                } else if line.starts_with('[') && !line.contains("]: http") {
-                    // Use full paths for documentation links
-                    Some((
-                        LibRsDocsLine::No(j + 1),
-                        line.replace("]: ", &("]: ".to_string() + &doc_root_url)),
-                    ))
-                } else {
-                    Some((LibRsDocsLine::No(j + 1), line.to_string()))
-                }
-            })
-            .map(|(j, line)| {
-                (
-                    j,
-                    line.replace(
-                        &format!(
-                            "https://github.com/{}/{}/blob/master/",
-                            crate_author, pkg_name
-                        ),
-                        "",
-                    ),
-                )
-            }),
-    );
-
-    for ((lhs_j, lhs), (rhs_j, rhs)) in readme_docs.iter().zip(lib_docs.iter()) {
-        assert_eq!(
-            lhs, rhs,
-            "lib.rs README.md and docs match failed on README.md line {} and lib.rs line \"{:?}\"",
-            lhs_j, rhs_j,
-        );
-    }
+    assert_sync(&readme, &docs);
 }

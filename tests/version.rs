124942c2b6c794f58c18a29da23bd22e9d8d9249
Andrey Zheleznov <zheland.net@gmail.com>
2026-01-13 18:33:56 +0300

    Fix preliminary version doc tests

--- tests/version.rs
+++ tests/version.rs
@@ -4,18 +4,24 @@
 #[cfg(all(test, not(miri)))]
 #[test]
 fn test_readme_deps() {
-    version_sync::assert_markdown_deps_updated!("README.md");
+    if !env!("CARGO_PKG_VERSION").contains("-pre") {
+        version_sync::assert_markdown_deps_updated!("README.md");
+    }
 }
 
 #[cfg(all(test, not(miri)))]
 #[test]
 fn test_html_root_url() {
-    version_sync::assert_html_root_url_updated!("src/lib.rs");
+    if !env!("CARGO_PKG_VERSION").contains("-pre") {
+        version_sync::assert_html_root_url_updated!("src/lib.rs");
+    }
 }
 
 #[cfg(all(test, not(miri)))]
 #[test]
 fn test_changelog_mentions_version() {
-    version_sync::assert_contains_regex!("CHANGELOG.md", "^## \\[{version}\\] - ");
-    version_sync::assert_contains_regex!("CHANGELOG.md", "\\[{version}\\]: ");
+    if !env!("CARGO_PKG_VERSION").contains("-pre") {
+        version_sync::assert_contains_regex!("CHANGELOG.md", "^## \\[{version}\\] - ");
+        version_sync::assert_contains_regex!("CHANGELOG.md", "\\[{version}\\]: ");
+    }
 }
